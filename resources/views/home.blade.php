26f5221b73b2f448612552e96931c06feb00cde2
muarief31 <200705088@student.ar-raniry.ac.id>
2022-11-11 19:11:38 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,202 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Resume/CV Design</title>
+	<link rel="stylesheet" href="stylescv.css">
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+</head>
+<body>
+
+<div class="resume">
+   <div class="resume_left">
+     <div class="resume_profile">
+       <img src="arif.JPG" alt="profile_pic">
+     </div>
+     <div class="resume_content">
+       <div class="resume_item resume_info">
+         <div class="title">
+           <p class="bold">Muhammad Arif</p>
+           <p class="regular">Mahasiswa</p>
+         </div>
+         <ul>
+           <li>
+             <div class="icon">
+               <i class="fas fa-map-signs"></i>
+             </div>
+             <div class="data">
+               Peureulak, Aceh Timur, Aceh <br /> Indonesia
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-mobile-alt"></i>
+             </div>
+             <div class="data">
+               +6285261009038
+             </div>
+           </li>
+           <li>
+             <div class="icon">
+               <i class="fas fa-envelope"></i>
+             </div>
+             <div class="data">
+               arif@gmail.com
+             </div>
+           </li>
+           
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">skill's</p>
+         </div>
+         <ul>
+           <li>
+             <div class="skill_name">
+               HTML
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               CSS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               Corel
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               JS
+             </div>
+             <div class="skill_progress">
+               <span style="width: 20%;"></span>
+             </div>
+             <div class="skill_per">20%</div>
+           </li>
+           <li>
+             <div class="skill_name">
+               PHP
+             </div>
+             <div class="skill_progress">
+               <span style="width: 15%;"></span>
+             </div>
+             <div class="skill_per">15%</div>
+           </li>
+         </ul>
+       </div>
+       <div class="resume_item resume_skills">
+         <div class="title">
+           <p class="bold">Social</p>
+         </div>
+         <ul>
+          <li>
+            <li>      
+            <div class="skill_name">
+                    Indonesia
+                  </div>
+                  <div class="skill_progress">
+                    <span style="width: 80%;"></span>
+                  </div>
+                  <div class="skill_per">80%</div>
+                </li>
+              <li>
+                <div class="skill_name">
+                  English
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 20%;"></span>
+                </div>
+                <div class="skill_per">20%</div>
+              </li>
+              <li>
+                <div class="skill_name">
+                  Aceh
+                </div>
+                <div class="skill_progress">
+                  <span style="width: 100%;"></span>
+                </div>
+                <div class="skill_per">100%</div>
+              </li>
+          </li>
+        </ul>
+       </div>
+     </div>
+  </div>
+  <div class="resume_right">
+    <div class="resume_item resume_about">
+        <div class="title">
+           <p class="bold">About us</p>
+         </div>
+        <p>My Name is Muhammad Arif, I'm 20 Years Old, I From Aceh, Indonesia</p>
+        <p>I am a university student from Ar-Raniry State Islamic University</p> 
+         <p> My Hoby is Futsal, and Watching Football</p> 
+    </div>
+    <div class="resume_item resume_work">
+        <div class="title">
+           <p class="bold">Education</p>
+         </div>
+        <ul>
+            <li>
+              <div class="date">2017 - 2020</div>
+              <div class="info">
+                     <p class="semi-bold">SMAN 1 PEUREULAK</p> 
+                  <p>Major: Sosial Science (IPS) </p>
+                </div>
+            </li>
+            <li>
+              <div class="date">2020 - NOW </div>
+              <div class="info">
+                     <p class="semi-bold">Ar-Raniry State Islamic University, Aceh</p> 
+                  <p>Major: Information Technology  </p>
+                </div>
+            </li>
+            
+          </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_education">
+      <div class="title">
+           <p class="bold">Work Experience</p>
+         </div>
+      <ul>
+            <li>
+              <div class="date">2020 - NOW</div>
+              <div class="info">
+                <p class="semi-bold">Student of Information Technology, Ar-Raniry State Islamic University</p> 
+                  
+                </div>
+            </li>
+        </ul>
+    </div>
+    <div class="resume_item resume_hobby">
+      <div class="title">
+           <p class="bold">Hobby</p>
+         </div>
+       <ul>
+         <li><i class="fas fa-book"></i></li>
+         <li><i class="fas fa-tv"></i></li>
+         <li><i class="fas fa-music"></i></li>
+         <li><i class="fas fa-futbol"></i></li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
